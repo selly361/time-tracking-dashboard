55235f267d8b7b2831f519ed77ecf7116fcf8e29
selly361 <seikou615@gmail.com>
2024-01-15 17:49:54 +0000

    Add: `index.ts` file to export svg icons

---
+++ src/Icons/index.ts
@@ -0,0 +1,10 @@
+/* Icons */
+
+export { ReactComponent as EllipsisIcon } from './icon-ellipsis.svg'
+export { ReactComponent as ExerciseIcon } from './icon-exercise.svg'
+export { ReactComponent as PlayIcon } from './icon-play.svg'
+export { ReactComponent as SelfCareIcon } from './icon-self-care.svg'
+export { ReactComponent as SocialIcon } from './icon-social.svg'
+export { ReactComponent as StudyIcon } from './icon-study.svg'
+export { ReactComponent as WorkIcon } from './icon-work.svg'
+export { ReactComponent as DotsIcon } from './icon-dots.svg'
